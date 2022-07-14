18919302d909ed286d5274950dd600262cc516ae
Keegan <leary.keegan@gmail.com>
2022-07-14 09:54:54 -0700

    login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,39 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
 import { useAuth } from "../hooks/useAuth";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
   const { request, logIn, loading } = useAuth();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <View>
-      <Text>{loading ? "loading..." : "Login to the app"}</Text>
-      <Button disabled={!request} title="Login" onPress={() => logIn()} />
+    <View className="flex-1">
+      <ImageBackground
+        className="flex-1 object-cover items-center"
+        source={{
+          uri: "https://tinder.com/static/tinder.png",
+        }}>
+        <TouchableOpacity
+          className="absolute bottom-40 bg-white rounded-2xl p-4"
+          onPress={() => logIn()}>
+          <Text className="font-semibold">Sign in & get swiping</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+      {/* <Text>{loading ? "loading..." : "Login to the app"}</Text>
+      <Button disabled={!request} title="Login" onPress={() => logIn()} /> */}
     </View>
   );
 };
