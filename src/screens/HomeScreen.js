39e24a7129f181695788e2667954a7caf6ed28ff
Keegan <leary.keegan@gmail.com>
2022-07-14 10:31:06 -0700

    header on home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,46 @@
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { useAuth } from "../hooks/useAuth";
+import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
+import Swiper from "react-native-deck-swiper";
 
 const HomeScreen = () => {
-  const navigator = useNavigation();
-  const { logOut } = useAuth();
+  const navigation = useNavigation();
+  const { user, logOut } = useAuth();
 
   return (
-    <View>
-      <Text className="text-red-500">This is HomeScreen</Text>
-      <Button
-        title="Go to Chat Screen"
-        onPress={() => navigator.navigate("Chat")}
-      />
-      <Button title="Logout" onPress={logOut} />
-    </View>
+    <SafeAreaView>
+      {/* Header */}
+      <View className="relative flex-row items-center justify-between px-4">
+        <TouchableOpacity className="" onPress={logOut}>
+          <Image
+            className="h-10 w-10 rounded-full"
+            source={{ uri: user.photoURL }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image className="w-14 h-14" source={require("../../logo.png")} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
+          <Ionicons
+            className=""
+            color="#FF5864"
+            size="30"
+            name="chatbubbles-sharp"
+          />
+        </TouchableOpacity>
+      </View>
+
+      {/* Swiper */}
+    </SafeAreaView>
   );
 };
 
